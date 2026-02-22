e57f3a272fc28e908f29747b95d2fb21ba871727
Maksym <112901851+Maksym82@users.noreply.github.com>
2026-02-22 14:46:55 +0200

    Update styles slider

--- js/main.js
+++ js/main.js
@@ -86,8 +86,9 @@ const swiper = new Swiper('.swiper', {
   direction: 'horizontal',
   loop: true,
 
-  // If we need pagination
-  pagination: {
-    el: '.swiper-pagination',
+  // Navigation arrows
+  navigation: {
+    nextEl: '.my-next',
+    prevEl: '.my-prev',
   },
 });
